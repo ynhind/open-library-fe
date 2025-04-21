5ca16cd180b41b92587ce1f494506af1516c16e7
ynhind <yennhi.nguyenduc@gmail.com>
2025-04-21 14:48:56 +0700

    origin main

--- src/Components/Footer/Footer.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import Logo from "../../assets/logo.png";
-export default function Footer() {
-  return <div>Footer</div>;
-}
